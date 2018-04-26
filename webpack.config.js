2ae01378ad9e318924379436d9904f5cae3d0f78
Sean McClure <sean.mcclure@colorado.edu>
2018-04-26 13:49:02 -0600

    Website updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,92 +2,85 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const buildName = 'keen-explorer';
-const minExt = (process.env.NODE_ENV === 'production' ? 'min.' : '');
+const minExt = process.env.NODE_ENV === 'production' ? 'min.' : '';
 
 const devModule = {
-  entry: {
-    'app': ['./client/js/app/app.js', './client/styles/base.less']
-  },
+    entry: {
+        app: ['./client/js/app/app.js', './client/styles/base.less']
+    },
 
-  output: {
-    filename: buildName + '.' + minExt + 'js',
-    path: './dist/'
-  },
+    output: {
+        filename: buildName + '.' + minExt + 'js',
+        path: './dist/'
+    },
 
-  module: {
-    loaders: [
-      {
-        test: /picker/,
-        loader: 'imports?define=>false'
-      },
+    module: {
+        loaders: [
+            {
+                test: /picker/,
+                loader: 'imports?define=>false'
+            },
 
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['latest', 'react'],
-        }
-      },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['latest', 'react']
+                }
+            },
 
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css!less')
-      }
-    ]
-  },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css!less')
+            }
+        ]
+    },
 
-  plugins: [
-    new ExtractTextPlugin(buildName + "." + minExt + "css"),
-  ],
+    plugins: [new ExtractTextPlugin(buildName + '.' + minExt + 'css')],
 
-  externals: {
-    jquery: 'jQuery',
-  }
-}
+    externals: {
+        jquery: 'jQuery'
+    }
+};
 
 const testModule = {
-  entry: {
-    'tests': './test/unit/index.js'
-  },
+    entry: {
+        tests: './test/unit/index.js'
+    },
 
-  output: {
-    filename: 'browserified_tests.js',
-    path: './test/unit/build/'
-  },
+    output: {
+        filename: 'browserified_tests.js',
+        path: './test/unit/build/'
+    },
 
-  module: {
-    loaders: [
-      {
-        test: /picker/,
-        loader: 'imports?define=>false'
-      },
+    module: {
+        loaders: [
+            {
+                test: /picker/,
+                loader: 'imports?define=>false'
+            },
 
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['latest', 'react'],
-        }
-      },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['latest', 'react']
+                }
+            },
 
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css!less')
-      }
-    ],
-    noParse: [
-      /\/sinon\.js/
-    ]
-  },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css!less')
+            }
+        ],
+        noParse: [/\sinon\.js/]
+    },
 
-  externals: {
-    jquery: 'jQuery',
-  }
-}
+    externals: {
+        jquery: 'jQuery'
+    }
+};
 
-module.exports = [
-  devModule,
-  testModule
-];
+module.exports = [devModule, testModule];
